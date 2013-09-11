2945a82176cd828a0eea4fa89c0fce150be45287
flaflifle <flv.canino@gmail.com>
2013-09-11 16:50:00 +0200

    reload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   def edit
    @user = User.find(params[:id])
-   if @user.update_attributes(user_params)
+   if @user.update_attributes(params[:user])
      flash[:success] = "Profile updated"
      sign_in @user
      redirect_to @user
